5ce68b2bf1f9cf1469749e3d306573b06b968f15
dallasbmerck <dmerck@wgu.edu>
2022-04-29 11:40:32 -0700

    First Commit

--- src/controller/UpdateCustmerController.java
+++ src/controller/UpdateCustmerController.java
@@ -1,4 +1,36 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class UpdateCustmerController {
+    public Label customerIDLabel;
+    public Label countryLabel;
+    public Label divisionLabel;
+    public Label nameLabel;
+    public Label addressLabel;
+    public Label postCodeLabel;
+    public Label phoneLable;
+    public TextField customerIDTextBox;
+    public ComboBox countryComboBox;
+    public ComboBox divisionComboBox;
+    public TextField nameTextBox;
+    public TextField addressTextBox;
+    public TextField postCodeTextBox;
+    public TextField phoneTextBox;
+    public Button saveButton;
+    public Button clearButton;
+    public Button backButton;
+
+    public void clickSaveButton(ActionEvent actionEvent) {
+    }
+
+    public void clickClearButton(ActionEvent actionEvent) {
+    }
+
+    public void clickBackButton(ActionEvent actionEvent) {
+    }
 }
