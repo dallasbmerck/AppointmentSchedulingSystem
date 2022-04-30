4726e5d0ef9abfef4bfa8a97a45a8fb5f61e40aa
dallasbmerck <dmerck@wgu.edu>
2022-04-30 10:07:37 -0700

    First Commit

--- src/controller/ReportingController.java
+++ src/controller/ReportingController.java
@@ -1,4 +1,23 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.ToggleGroup;
+
 public class ReportingController {
+    public TextArea reportsTextArea;
+    public RadioButton reportMandTbutton;
+    public ToggleGroup toggleGroup;
+    public RadioButton contactScheduleButton;
+    public RadioButton totalByCountry;
+
+    public void clickReportMandTbutton(ActionEvent actionEvent) {
+    }
+
+    public void clickContactScheduleButton(ActionEvent actionEvent) {
+    }
+
+    public void clickTotalByCountry(ActionEvent actionEvent) {
+    }
 }
